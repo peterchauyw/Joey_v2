40f10fab877fee34494196a8843dd24aaefa8146
osvaldorj <os7rj89@gmail.com>
2016-07-02 22:30:55 -0700

    Frontend changes
    Changes in the first tab screen.

--- app/src/main/java/com/example/os/joey_beta/HomeFragment.java
+++ app/src/main/java/com/example/os/joey_beta/HomeFragment.java
@@ -1,17 +1,23 @@
 package com.example.os.joey_beta;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.os.joey_beta.Services.BluetoothService;
+
+import org.w3c.dom.Text;
 
 /**
  * Created by OS on 10/05/2016.
  */
 public class HomeFragment extends Fragment{
+
+    TextView txtDistance;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -20,8 +26,17 @@ public class HomeFragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        txtDistance = (TextView)view.findViewById(R.id.txtDistance);
+        txtDistance.setText("10 ft");
+
+        //return inflater.inflate(R.layout.fragment_home, container, false);
+        return view;
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
 }
